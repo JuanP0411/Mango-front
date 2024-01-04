38727263690e616c993e3873877f401b32f1d4ab
Juan Pablo Herrera <j.herrera@lah-group.net>
2024-01-04 16:56:21 -0500

    Provided file validation and improved user experience

--- Home.py
+++ Home.py
@@ -13,29 +13,34 @@ image = st.image(image='mango.png',caption='uploaded image')
 uploaded_file = st.file_uploader("Choose a file")
 
 if uploaded_file is not None:
-    url = 'http://127.0.0.1:5555/get-predict'
+    url = 'https://mango-classifier-api.onrender.com/get-predict'
+    if uploaded_file.type == 'image/jpeg' :
 
-    file_bytes = uploaded_file.getvalue()
+        file_bytes = uploaded_file.getvalue()
+        
 
+        with st.spinner('Loading AI model'):
+            response = requests.post(
+            url=url,
+            files={'file':file_bytes}
+            )
     
-    response = requests.post(
-        url=url,
-        files={'file':file_bytes}
-    )
-    image.image(uploaded_file.getvalue(),caption='uploaded image')
-    image_name_json = json.loads(response.text)
-    st.session_state.name = image_name_json
- 
+        image.image(uploaded_file.getvalue(),caption='uploaded image')
+        image_name_json = json.loads(response.text)
+        st.session_state.name = image_name_json
+    else : 
+        st.warning(f"We do not support {uploaded_file.type}, currently we only suport .jpeg images")
 
 
-if st.session_state.name != "":
-    st.write("diseases = ", st.session_state.name)
-
+if st.session_state.name != "" and st.session_state.name != "Healthy":
+    st.write("Detected diseases: ", st.session_state.name)
+elif st.session_state.name == "Healthy" :
+    st.write("Your plant is healthy :)")
 
 if "messages" not in st.session_state.keys():
     st.session_state.messages = [{"role": "assistant", "content": "How may I assist you today?"}]
 
-if st.session_state.name != "": 
+if st.session_state.name != "" and st.session_state.name != "Healthy": 
     mango_disease = st.session_state.name
     temp_prompt = f"I have a mango crop with {mango_disease} how should I treat it, and what could be causing the diseases"
     st.session_state.messages.append({"role": "user", "content": temp_prompt})
